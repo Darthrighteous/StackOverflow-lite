d33eb1962e8774fc095bd7d9dd6fe18b9964f1a6
Jalil <ogunniyiabduljalil@yahoo.com>
2018-08-29 04:47:34 +0100

    [Feature #159952061] add get one question endpoint

--- server/controllers/db/queries.js
+++ server/controllers/db/queries.js
@@ -22,3 +22,26 @@ export const getAllQuestions = async (req, res) => {
     res.status(404);
   }
 };
+
+/**
+* perform database query to get one question
+* @param {object} req The request
+* @param {object} res The response
+* @param {object} next To pass onto next route
+* @returns {void}
+*/
+export const getOneQuestion = async (req, res, next) => {
+  const id = req.params.questionId;
+  try {
+    const question = await db.one(`SELECT * FROM questions WHERE id=${id}`);
+    res.status(200).json({
+      status: 'success',
+      message: 'one question retrieved successfully',
+      question,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(404);
+    next(new Error(`Question of that id= ${id} not found`));
+  }
+};
--- server/spec/controllers/db/get.spec.js
+++ server/spec/controllers/db/get.spec.js
@@ -1,4 +1,5 @@
 import Request from 'request';
+import { questionInput } from './utils';
 
 const data = {};
 describe('GET ROUTE', () => {
@@ -15,5 +16,35 @@ describe('GET ROUTE', () => {
     it('has status 200', () => {
       expect(data.status).toBe(200);
     });
+  });
+
+  describe('for one questions', async () => {
+    // TODO: TEST ONCE POST METHOD IS CREATED
+    // let qId;
+    // // post a question
+    // beforeAll((done) => {
+    //   Request({ url: 'http://localhost:4001/v2/questions/', method: 'POST', json: questionInput }, (error, response, body) => {
+    //     qId = body.id;
+    //     done();
+    //   });
+    // });
+    // // get the question posted
+    // beforeAll((done) => {
+    //   Request.get(`http://localhost:4001/v2/questions/${qId}`, (error, response, body) => {
+    //     data.status = response.statusCode;
+    //     data.question = JSON.parse(body).question;
+    //     done();
+    //   });
+    // });
+    // // get query successfull
+    // it('has status 200', () => {
+    //   expect(data.status).toBe(200);
+    // });
+    // // question has posted values
+    // it('has expected body', () => {
+    //   expect(data.question.title).toBe(questionInput.title);
+    //   expect(data.question.body).toBe(questionInput.body);
+    //   expect(data.question.username).toBe(questionInput.username);
+    // });
   });
 });
