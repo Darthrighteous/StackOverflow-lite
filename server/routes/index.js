5429c9c7f2f43e7b4688ee01e1ef585a3580e610
Jalil <ogunniyiabduljalil@yahoo.com>
2018-08-29 13:23:35 +0100

    [Chore #160130997] remove dummy data routes and functions

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,5 @@
 import express from 'express';
 import {
-  getAllQuestionsx,
-  getOneQuestionx,
-  validQuestionx,
-  validQueryx,
-  postQuestionx,
-  postAnswerx,
-  acceptAnswerx,
-  editQuestionx,
-  deleteQuestionx,
-} from '../controllers';
-
-import {
   getAllQuestions,
   getOneQuestion,
   postQuestion,
@@ -29,21 +17,6 @@ import {
 
 const router = express.Router();
 
-// validate question Id
-router.use('/v1/questions/:questionId', validQuestionx);
-router.use('/v1/questions', validQueryx);
-
-// DUMMY DATA ROUTES
-router.get('/v1/questions', getAllQuestionsx);
-router.get('/v1/questions/:questionId', getOneQuestionx);
-
-router.post('/v1/questions', postQuestionx);
-router.post('/v1/questions/:questionId/answers', postAnswerx);
-
-router.patch('/v1/questions/:questionId/answers/:answerId', acceptAnswerx);
-router.patch('/v1/questions/:questionId', editQuestionx);
-router.delete('/v1/questions/:questionId', deleteQuestionx);
-
 // DATABASE ROUTES
 router.get('/v2/questions', getAllQuestions); // get all questions
 router.get('/v2/questions/:questionId', getOneQuestion); // get a single question
@@ -54,7 +27,6 @@ router.post('/v2/questions/:questionId/answers', verifyJWT, postAnswer); // post
 router.delete('/v2/questions/:questionId', verifyJWT, deleteQuestion); // delete question
 
 router.patch('/v2/questions/:questionId/answers/:answerId', verifyJWT, acceptAnswer); // accept answer
-
 
 // AUTH ROUTES
 router.post('/v2/auth/signup', signUp);
