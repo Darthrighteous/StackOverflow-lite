5a8ba52acb166b5d6176550ea95e9b17c6d2c9b2
Jalil <ogunniyiabduljalil@yahoo.com>
2018-08-30 16:03:10 +0100

    add database set up

--- server/controllers/db/queryUtils.js
+++ server/controllers/db/queryUtils.js
@@ -1,10 +1,7 @@
 import promise from 'bluebird';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 import pgpmodule from 'pg-promise';
-import {
-  questions,
-  answers,
-  users,
-} from './sql';
 
 const options = {
   // Initialization Options
@@ -12,40 +9,90 @@ const options = {
 };
 const pgp = pgpmodule(options);
 
-const connectionString = 'postgres://Admin:a@localhost:5432/Stackoverflow-lite';
+// const connectionString = 'postgres://Admin:a@localhost:5432/Stackoverflow-lite';
+const connectionString = 'postgres://uvpmkfrmhhtjat:7e69cc7451b2fa93566bed21eee2428b8e8225ac3f43bcfbc4df445f7849ace9@ec2-54-83-51-78.compute-1.amazonaws.com:5432/deomppek79o3jn'
 export const db = pgp(connectionString);
 
+dotenv.config();
+
+const pool = new Pool({
+  // connectionString: process.env.DATABASE_URL,
+  connectionString,
+});
+
 /**
-* initialize all tables
+*
 */
-export const initAllTables = () => {
-  db.none(questions.drop);
-  db.none(questions.create);
-  db.none(questions.seed);
+export const initTables = () => {
+  const queryText = `
+  DROP TABLE 
+  IF EXISTS questionss;
 
-  db.none(answers.drop);
-  db.none(answers.create);
-  db.none(answers.seed);
+  CREATE TABLE questionss (
+    id serial PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    body VARCHAR,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR (30) NOT NULL,
+    score INTEGER DEFAULT 0,
+    answer_count INTEGER DEFAULT 0,
+    accepted_answer INTEGER DEFAULT -1);
 
-  db.none(users.drop);
-  db.none(users.create);
-  db.none(users.seed);
+  INSERT INTO
+  questionss (title, body, username)
+  VALUES
+  ('question 1','body 1','user1'),
+  ('question 2','body 2','user2'),
+  ('question 3','body 3','user3'),
+  ('question 4','body 4','user4'),
+  ('question 5','body 5','user5'),
+  ('question 6','body 6','user6');
+
+  DROP TABLE 
+  IF EXISTS answerss;
+
+  CREATE TABLE answerss (
+    id serial PRIMARY KEY,
+    body VARCHAR,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR (30) NOT NULL,
+    accepted BOOLEAN DEFAULT FALSE,
+    score INTEGER DEFAULT 0,
+    question_id INTEGER DEFAULT -1);
+
+  INSERT INTO
+  answerss (body, username, question_id)
+  VALUES
+  ('body 1','user1', 1),
+  ('body 2','user2', 2),
+  ('body 3','user3', 3),
+  ('body 4','user4', 4),
+  ('body 5','user5', 5),
+  ('body 6','user6', 6);
+  
+  DROP TABLE 
+  IF EXISTS userss;
+
+  CREATE TABLE userss (
+    id serial PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR,
+    username VARCHAR (30) NOT NULL,
+    password VARCHAR (60) NOT NULL)`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
 };
 
-/**
-* creates a question table
-*/
-export const createQuestionTable = () => db.none(questions.create);
-
-/**
-* seeds the question table
-*/
-export const seedQuestionTable = () => db.none(questions.seed);
-
-/**
-* drop the question table
-*/
-export const dropQuestionTable = () => db.none(questions.drop);
+initTables();
 
 /**
  * validates the body of a question(POST).
