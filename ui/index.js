dc984ef0480a225d04a7d0716b41a115144b8bdb
Jalil <ogunniyiabduljalil@yahoo.com>
2018-09-27 10:44:05 +0100

    [Feature #160808372] fix search bar on all pages

--- ui/index.js
+++ ui/index.js
@@ -11,16 +11,3 @@ fetchAllQuestions(getAllUrl);
 const indexSortUrl = `${baseUrl}/questions?sort`;
 // index sort drop down setup
 dropDownListenerInit('indexDropdown', indexSortUrl);
-
-/**
-*
-*
-*/
-const search = () => {
-  const searchString = document.getElementById('search_bar').value;
-  console.log(searchString);
-};
-
-const form = document.getElementById('search_form');
-console.log(form);
-form.addEventListener('submit', search);
