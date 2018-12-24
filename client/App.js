2ca00dd6c4a29bd1a1fe8d5c132e9d7471d84b16
darthrighteous <abdul-jalil.ogunniyi@andela.com>
2018-12-25 00:56:34 +0100

    [Chore #162828524] setup react router
    [Finishes #162828524]

--- client/App.js
+++ client/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import NotFound from './components/NotFound'
 
 const App = () => (
-  <div>
-    <h1>StackOverflow-Lite</h1>
-  </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      {/* <Route path="/signup" component={Signup}/> */}
+      {/* <Route path="/login" component={Login}/> */}
+      {/* <Route path="/new-question" component={NewQuestion}/> */}
+      {/* <Route path="/question/:id" component={Question}/> */}
+      <Route component={NotFound}/>
+    </Switch>
+  </Router>
 );
 
 export default App;
